c54f4975ce087222f7bc5b992c685ccc08cec172
JarekParal <paral.jarek@gmail.com>
2016-08-08 13:44:41 +0200

    Add example with variables

---
+++ tahaky/variables.ino
@@ -0,0 +1,29 @@
+/*
+   Example variables and serial communication
+   Created by Jaroslav Páral
+   RoboticsCamp 2016
+   RoboticsBrno
+   www.robotikabrno.cz
+   
+   Popis:
+   Ukazkove priklady na praci s promenyma a seriovou linku.
+ */
+
+void setup() {
+  Serial.begin(9600);
+}
+
+bool pravdaNepravda; // 0 nebo 1
+int celeCislo = 42; // -32768 <--> +32767 
+float desetineCislo = 3.14; //  -3.4028235E+38 <--> 3.4028235E+38 
+char znak = 'a'; // znak se zapisuje do apostrofu
+char poleZnaku[] = "ahoj"; // textovy retezec se zapisuje do uvozovek
+
+void loop() {
+  Serial.println(pravdaNepravda);
+  Serial.println(celeCislo);
+  Serial.println(desetineCislo);
+  Serial.println(znak);
+  Serial.println(); // vytiskne prazdny novy radek
+  delay(100);
+}
