58a2c12fee062a44c5257d0df28503caf1812bd5
JarekParal <paral.jarek@gmail.com>
2016-08-09 19:55:14 +0200

    Update serialLedBlinkingOnOff.ino

--- tahaky/serialLedBlinkingOnOff.ino
+++ tahaky/serialLedBlinkingOnOff.ino
@@ -9,31 +9,36 @@
    Ovladani blikani ledky pomoci seriove linky.
  */
 
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(9600);
+void setup() 
+{
+  pinMode(LED_BUILTIN, OUTPUT); // nastaveni pinu LED_BUILTIN na vystupni
+  Serial.begin(9600); // zapnuti seriove linky
 }
 
-char prijimanyZnak;
-bool blikaniZapnutoVypnuto = false; // false = 0
+char prijimanyZnak; // promena pro prijimani znaku po seriove lince
+bool blikaniZapnutoVypnuto = false; // false == 0; true != 0 (napr. -4, 2, 1000)
 
-void loop() {
-  if (Serial.available()) {
-    prijimanyZnak = Serial.read();
+void loop() 
+{
+  if (Serial.available()) 
+  {
+    prijimanyZnak = Serial.read();  // cteni znaku ze seriove linky
 
-    if(prijimanyZnak == 'z' || prijimanyZnak == 'Z') {
+    if(prijimanyZnak == 'z' || prijimanyZnak == 'Z') // porovnani prijateho znaku
+    {
       blikaniZapnutoVypnuto = true;
       Serial.println("Blikani - zapnuto ('z')");
     }
-    else if(prijimanyZnak == 'v' || prijimanyZnak == 'V') {
+    else if(prijimanyZnak == 'v' || prijimanyZnak == 'V') 
+    {
       blikaniZapnutoVypnuto = false;
-      Serial.println("Blikani - vypnuto ('v')");
-    }
+      Serial.println("Blikani - vypnuto ('v')"); 
+    }    
     else
       Serial.println("Neznamy prikaz");
   }  
-
-  if(blikaniZapnutoVypnuto == true) {
+  if(blikaniZapnutoVypnuto == true) 
+  {
     digitalWrite(LED_BUILTIN, HIGH);
     delay(500);
     digitalWrite(LED_BUILTIN, LOW);
