715aa05424289cc78577fd56976a38804f0311af
JarekParal <paral.jarek@gmail.com>
2016-08-09 20:03:44 +0200

    Add example for led blinking and changing freq. by serial com.

---
+++ tahaky/serialLedBlinkingFreq.ino
@@ -0,0 +1,58 @@
+/*
+   Example led blinking (on/off + freq) by serial communication
+   Created by Jaroslav Páral
+   RoboticsCamp 2016
+   RoboticsBrno
+   www.robotikabrno.cz
+   
+   Popis:
+   Ovladani blikani ledky a jeji frekvence pomoci seriove linky.
+ */
+
+void setup() 
+{
+  pinMode(LED_BUILTIN, OUTPUT); // nastaveni pinu LED_BUILTIN na vystupni
+  Serial.begin(9600); // zapnuti seriove linky
+}
+
+char prijimanyZnak; // promena pro prijimani znaku po seriove lince
+bool blikaniZapnutoVypnuto = false; // false == 0; true != 0 (napr. -4, 2, 1000)
+int delkaPauzy = 500; // promena pro ovlivnovani frekvence blikani (v milisekundach) 
+
+void loop() 
+{
+  if (Serial.available()) 
+  {
+    prijimanyZnak = Serial.read();  // cteni znaku ze seriove linky
+
+    if(prijimanyZnak == 'z' || prijimanyZnak == 'Z') // porovnani prijateho znaku
+    {
+      blikaniZapnutoVypnuto = true;
+      Serial.println("Blikani - zapnuto ('z')");
+    }
+    else if(prijimanyZnak == 'v' || prijimanyZnak == 'V') 
+    {
+      blikaniZapnutoVypnuto = false;
+      Serial.println("Blikani - vypnuto ('v')"); 
+    }    
+    else if(prijimanyZnak == 'p' || prijimanyZnak == 'P') 
+    {
+      delkaPauzy = delkaPauzy + 100; // delkaPauzy += 100;
+      Serial.println("Blikani - pomaleji ('p')"); 
+    }    
+    else if(prijimanyZnak == 'r' || prijimanyZnak == 'R') 
+    {
+      delkaPauzy = delkaPauzy - 100; // delkaPauzy -= 100;
+      Serial.println("Blikani - rychleji ('r')"); 
+    }
+    else
+      Serial.println("Neznamy prikaz");
+  }  
+  if(blikaniZapnutoVypnuto == true) 
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(delkaPauzy);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(delkaPauzy);
+  }
+}
