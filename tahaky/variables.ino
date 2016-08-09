d717691f9e5b360a57df55f3d085aa222a467bce
JarekParal <paral.jarek@gmail.com>
2016-08-09 13:34:43 +0200

    Update variables.ino

--- tahaky/variables.ino
+++ tahaky/variables.ino
@@ -9,21 +9,24 @@
    Ukazkove priklady na praci s promenyma a seriovou linku.
  */
 
-void setup() {
+void setup() 
+{
   Serial.begin(9600);
 }
 
-bool pravdaNepravda; // 0 nebo 1
+bool pravdaNepravda = false; // false nebo true (0 nebo 1)
 int celeCislo = 42; // -32768 <--> +32767 
 float desetineCislo = 3.14; //  -3.4028235E+38 <--> 3.4028235E+38 
 char znak = 'a'; // znak se zapisuje do apostrofu
 char poleZnaku[] = "ahoj"; // textovy retezec se zapisuje do uvozovek
 
-void loop() {
+void loop() 
+{
   Serial.println(pravdaNepravda);
   Serial.println(celeCislo);
   Serial.println(desetineCislo);
   Serial.println(znak);
+  Serial.println(poleZnaku);
   Serial.println(); // vytiskne prazdny novy radek
   delay(100);
 }
